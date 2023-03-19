d57a8fd389513371eace2c11cc63fbb92cb0bc86
ShodiyevF <shodiyevf@yandex.ru>
2023-03-19 16:29:04 +0500

    initial commit

---
+++ src/app.js
@@ -0,0 +1,49 @@
+require('dotenv').config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
+
+const { errorMiddleware } = require('./middleware/error.widdleware.js');
+const setMockdata = require('./config/database.js');
+const { stream } = require('./config/logger.js');
+const CORS_OPTIONS = require('./config/cors.js');
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+async function app(routes) {
+    const app = express();
+    const port = process.env.PORT || 3000;
+    const action = process.env.NODE_ENV || 'development';
+
+    function lister() {
+        app.listen(port, () => {
+            console.info('=================================');
+            console.info(`======= ENV: ${action} =======`);
+            console.info(`🚀 App listening on the port ${port}`);
+            console.info('=================================');
+        });
+    }
+
+    function initializeMiddlewares() {
+        app.use(morgan(process.env.LOG_FORMAT, { stream }));
+        app.use(cors(CORS_OPTIONS));
+        app.use(express.json());
+        app.use(express.urlencoded({ extended: true }));
+        app.use(errorMiddleware);
+    }
+
+    function initializeRoutes(routes) {
+        routes.forEach(route => {
+            app.use(route);
+        });
+    }
+
+    async function runner() {
+        await setMockdata();
+        await initializeMiddlewares();
+        await initializeRoutes(routes);
+        await lister();
+    }
+
+    runner();
+}
+
+module.exports = app;
---
+++ src/config/dtoformat.js
@@ -0,0 +1,1 @@
+const dtoFormat = ['expose', 'isString', 'minLength', 'maxLength', 'boolean', 'notEqual'];
